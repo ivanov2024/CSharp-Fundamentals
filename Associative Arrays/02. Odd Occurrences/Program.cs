94ae9737ce257fe5bec6e7ca7571fa5b0aaf7699
Ivan Ivanov <ivanov.github@gmail.com>
2024-11-14 16:52:37 +0200

    Added tasks from lecture "Associative Arrays"

---
+++ Associative Arrays/02. Odd Occurrences/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string,int> counts = new Dictionary<string,int>();
+
+            foreach (string word in words)
+            {
+                string lowerWord = word.ToLower();
+                if (counts.ContainsKey(lowerWord))
+                {
+                    counts[lowerWord]++;
+                }
+                else
+                {
+                    counts.Add(lowerWord, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Associative Arrays/03. Word Synonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+           int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (words.ContainsKey(word) == false)
+                {
+                    words.Add(word,new List<string>());
+                }
+                    words[word].Add(synonym);
+            }
+
+            foreach (var kvp in words)
+            {
+                string word = kvp.Key;
+                List<string> synonymList = kvp.Value;
+                string synonymsAsString = string.Join(", ", synonymList);
+
+                Console.WriteLine($"{word} - {synonymsAsString}");
+            }
+        }
+    }
+}
